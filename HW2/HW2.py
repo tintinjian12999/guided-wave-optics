14e9dce39b4cd2e9b371fdd8362be1690ffc32fa
tintinjian <tintinjian@gmail.com>
2023-10-03 01:43:32 +0800

    Solve beta from solving eigenvalue of maxwell equ

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -1,27 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 a = 4E-6
-upper_bound = 4 * a  # Substrate to waveguide: a, waveguide: 2a, waveguide to cladding: 2a
+upper_bound =4 * a  # Substrate to waveguide: a, waveguide: 2a, waveguide to cladding: 2a
 n1 = 1.516
 ns = 1
 n0 = 1
-N = 120000 # It's better to be multiplies of 4
+N = 12000 # It's better to be multiplies of 4
 points = np.linspace(0, upper_bound, N)
 step = upper_bound / N  # Delta_x
 lambda1 = 1.55E-6
 k = 2 * np.pi / lambda1
 # Define the refractive index in each layer
+#O(N)
 refractive_index = []
-for i in range(int(N / 4)):
+for i in range(int( N / 4 )):
     refractive_index.append(ns)
-for i in range(int(N / 4) ,int(3 * N / 4)):
+for i in range(int(N /4 ) ,int( 3  * N/4 )):
     refractive_index.append(n1)
 for i in range(int(3 * N / 4) ,int(N)):
     refractive_index.append(n0)
-
 refractive_index = np.array(refractive_index)
 mu_n = (k ** 2) * (refractive_index ** 2) * (step ** 2) - 2
 array = np.zeros((N,N))
+#O(N)
 for i in range(N):
     if (i == 0):
         array[0][0] = mu_n[0] / (step ** 2)
@@ -33,17 +34,19 @@ for i in range(N):
         array[i][i - 1] = 1 / (step ** 2)
         array[i][i] = mu_n[i] / (step ** 2)
         array[i][i + 1] = 1 / (step ** 2)
+#O(N ^ 3) !!
 eigenvalues, eigenvectors = np.linalg.eig(array)
 filter = eigenvalues >= 0
 eigenvalues = eigenvalues[filter]
 eigenvectors = eigenvectors[filter]
-effective_refractive_index = np.sqrt(eigenvalues) / k
-filter = effective_refractive_index <= n1
+effective_refractive_index = np.sqrt(eigenvalues) 
+filter = effective_refractive_index <= n1 * k
 eigenvectors = eigenvectors[filter]
 effective_refractive_index = effective_refractive_index[filter]
-filter = effective_refractive_index >= ns
+filter = effective_refractive_index >= ns * k
 eigenvectors = eigenvectors[filter]
 effective_refractive_index = effective_refractive_index[filter]
-beta = [round(number * k, 3) for number in effective_refractive_index]
+beta = [round(number , 3) for number in effective_refractive_index]
+beta = np.sort(beta)
 print(beta)
 
